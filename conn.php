6041b3861b9ee87b1e126b501ea2f149697c665e
matinjun <44500594+matinjun@users.noreply.github.com>
2020-11-11 00:16:53 +0800

    注释

--- conn.php
+++ conn.php
@@ -3,6 +3,7 @@ $hostname = "localhost"; //主机名,可以用IP代替
 $database = "xiaoma"; //数据库名
 $username = "root"; //数据库用户名
 $password = ""; //数据库密码
+// php找不到数据库
 $conn = mysqli_connect($hostname, $username, $password, $database);// or trigger_error(mysql_error() , E_USER_ERROR); 
 if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
